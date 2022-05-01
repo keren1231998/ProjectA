e202711c6a96c9540ec9863f982ff18b45479096
ori fadida <orifad100@gmail.com>
2022-05-01 11:40:46 +0300

    Commited

--- ATP-Project-PartA/src/algorithms/search/SearchableMaze.java
+++ ATP-Project-PartA/src/algorithms/search/SearchableMaze.java
@@ -32,51 +32,12 @@ public class SearchableMaze implements ISearchable
         ArrayList<AState> neighbors =new ArrayList<AState>();//neighbors
         int row=((MazeState) s).getX();
         int column=((MazeState) s).getY();
-        int i;
-        int r1=row-1;
-        int cl1 = column-1;
         int rowmaze=maze1.getGoalPosition().getRowIndex();
         int colmaze=maze1.getGoalPosition().getColumnIndex();
-
-//        for (i=0; i<2;i++)
-//        {
-//            try {
-//                if(cl1<0||column>colmaze)continue;
-//            }
-//            catch (Exception e)
-//            {
-//                continue;
-//            }
-//            Position p1=new Position(r1 , cl1);
-//            AState a1=new MazeState(p1);
-//            neighbors.add(a1);
-//            cl1 = cl1 +2;
-//
-//
-//        }
-//         cl1 = column-1;
-//        r1=r1+2;
-//        for (i=0; i<2;i++){
-//            try {
-//                if(cl1<0||column>colmaze)continue;
-//
-//            }
-//            catch (Exception e){
-//                continue;
-//            }
-//            Position p1=new Position(r1 , cl1);
-//            AState a1=new MazeState(p1);
-//            neighbors.add(a1);
-//            cl1 = cl1 +2;
-//        }
-
-
 
         for (int x = -1; x <= 1; x++)
             for (int y = -1; y <= 1; y++)
             {
-//                if (x == 0 && y == 0 || x != 0 && y != 0)
-//                    continue;
                 try {
                     if (row+x<0||row+x>rowmaze||column+y<0||column+y>colmaze||(row+x==row&&column+y==column)) continue;
                 } catch (Exception e) { // ignore ArrayIndexOutOfBounds
@@ -88,7 +49,6 @@ public class SearchableMaze implements ISearchable
 
                 neighbors.add(a1);//add all neighbors to the list
             }
-
         return neighbors;
     }
 }
--- ATP-Project-PartA/src/com/company/Main.java
+++ ATP-Project-PartA/src/com/company/Main.java
@@ -1,40 +1,40 @@
-package com.company;
-import algorithms.mazeGenerators.*;
-import algorithms.search.AState;
-import algorithms.search.MazeState;
-import algorithms.search.SearchableMaze;
-
-public class Main {
-
-    public static void main(String[] args)
-    {
-
-
-
-
-	int maze1[][]={{0,0,1,1},
-                  {0,0,1,1},
-                  {0,0,1,1},
-                  {0,0,1,1}
-
-
-    };
-
-    Position start=new Position(0,0);
-    Position end=new Position(3,3);
-    Position check=new Position(3,3);
-    AState aState=new MazeState(check);
-    Maze m1=new Maze(start,end,maze1);
-    SearchableMaze search=new SearchableMaze(m1);
-    search.getAllSuccessors(aState);
+//package com.company;
+//import algorithms.mazeGenerators.*;
+//import algorithms.search.AState;
+//import algorithms.search.MazeState;
+//import algorithms.search.SearchableMaze;
 //
-//    SimpleMazeGenerator s1=new SimpleMazeGenerator();
-//   // EmptyMazeGenerator e1 = new EmptyMazeGenerator();
-//    Maze maze1=s1.generate(12,12);
+//public class Main {
+//
+//    public static void main(String[] args)
+//    {
 //
 //
-//    Maze maze1 =new Maze(start,end,maze);
-//    maze1.print();
-
-    }
-}
+//
+//
+//	int maze1[][]={{0,0,1,1},
+//                  {0,0,1,1},
+//                  {0,0,1,1},
+//                  {0,0,1,1}
+//
+//
+//    };
+//
+//    Position start=new Position(0,0);
+//    Position end=new Position(3,3);
+//    Position check=new Position(3,3);
+//    AState aState=new MazeState(check);
+//    Maze m1=new Maze(start,end,maze1);
+//    SearchableMaze search=new SearchableMaze(m1);
+//    search.getAllSuccessors(aState);
+////
+////    SimpleMazeGenerator s1=new SimpleMazeGenerator();
+////   // EmptyMazeGenerator e1 = new EmptyMazeGenerator();
+////    Maze maze1=s1.generate(12,12);
+////
+////
+////    Maze maze1 =new Maze(start,end,maze);
+////    maze1.print();
+//
+//    }
+//}
